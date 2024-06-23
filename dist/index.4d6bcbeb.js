324dcdaf00155c4c98b9a2fa38d4bbd6a37dcb17
juancmdev <juancmorenoj@gmail.com>
2024-06-23 18:43:12 -0500

    update

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -584,7 +584,16 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"gLLPy":[function(require,module,exports) {
-var _three = require("three");
+var _three = require("three"); /*
+Every three.js app you create will require all six of these steps, although more complex apps will often require many more.
+
+    1. Initial Setup
+    2. Create the Scene
+    3. Create the Camera
+    4. Create a Visible Object
+    5. Create the Renderer
+    6. Render the Scene
+*/ 
 
 },{"three":"ktPTu"}],"ktPTu":[function(require,module,exports) {
 /**
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import * as THREE from "three";
+
+/*
+Every three.js app you create will require all six of these steps, although more complex apps will often require many more.
+
+    1. Initial Setup
+    2. Create the Scene
+    3. Create the Camera
+    4. Create a Visible Object
+    5. Create the Renderer
+    6. Render the Scene
+*/
