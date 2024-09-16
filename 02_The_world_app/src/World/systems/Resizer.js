c38b34170887a924ce7cfe2d395e4810069263b8
juancmdev <juancmorenoj@gmail.com>
2024-09-15 21:23:53 -0500

    updated

--- 02_The_world_app/src/World/systems/Resizer.js
+++ 02_The_world_app/src/World/systems/Resizer.js
@@ -14,7 +14,12 @@ const setSize = (container, camera, renderer) => {
 
 class Resizer {
   constructor(container, camera, renderer) {
-    setSize = (container, camera, renderer);
+    setSize(container, camera, renderer);
+
+    window.addEventListener("resize", () => {
+      // set the size again if a resize occurs
+      setSize(container, camera, renderer);
+    });
   }
 }
 
--- dist/index.dad59a65.js
+++ dist/index.dad59a65.js
@@ -31866,7 +31866,11 @@ const setSize = (container, camera, renderer)=>{
 };
 class Resizer {
     constructor(container, camera, renderer){
-        setSize = (container, camera, renderer);
+        setSize(container, camera, renderer);
+        window.addEventListener("resize", ()=>{
+            // set the size again if a resize occurs
+            setSize(container, camera, renderer);
+        });
     }
 }
 
